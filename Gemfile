8de49f5032c0926c5f21b30fcb240c064cd1ec6c
Magnuss Karklins <magnuss.karklins@gmail.com>
2015-09-19 15:03:57 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.4'
+gem 'rails-api'
+gem 'sqlite3'
+gem 'spring', group: :development
+gem 'capistrano', group: :development
+gem 'pry-rails', group: :development
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Deploy with Capistrano
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+ShowList::Application.config.secret_token = '85923d0f7c056f9e6f80686db16777c3f6e5ca4e4e3e0a6537d5d193da1f9b634060676b1863c8340ad03fa2471674fb6b4fe8607a1a8e22203c2aba54de9f9f'
