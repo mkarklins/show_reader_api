8a080f3565bd8276fcd3ed7aece7d51c276047b4
Magnuss Karklins <magnuss.karklins@gmail.com>
2015-10-05 21:36:54 +0300

    Rename app

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ShowList
+module ShowReader
   class Application < Rails::Application
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-ShowList::Application.config.secret_token = '85923d0f7c056f9e6f80686db16777c3f6e5ca4e4e3e0a6537d5d193da1f9b634060676b1863c8340ad03fa2471674fb6b4fe8607a1a8e22203c2aba54de9f9f'
+ShowReader::Application.config.secret_token = '85923d0f7c056f9e6f80686db16777c3f6e5ca4e4e3e0a6537d5d193da1f9b634060676b1863c8340ad03fa2471674fb6b4fe8607a1a8e22203c2aba54de9f9f'
